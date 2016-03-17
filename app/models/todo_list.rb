db3d71fdd8df03f26d86769deb05915501cd0673
yramocan <yramocan@umich.edu>
2016-03-17 00:06:29 -0400

    Add validations for description

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, :description, presence: true
   validates :title, length: { minimum: 3 }
+  validates :description, length: { minimum: 5 }
 end
